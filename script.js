3e0e8e60a48302cfc337c2c783958ae20f25280b
cgbea <151942177+cgbea@users.noreply.github.com>
2024-01-28 19:55:32 +0000

    added click functionality to button history

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var APIKey = "c91d97b6ecd2f9c86a0a9473a725de0c";
 var storedCities = JSON.parse(localStorage.getItem("city")) || [];
 var search = $("#search-button");
 var clear = $("#clear-button");
-
+var cityButton =$(".btn-secondary")
 
 // Click event handler for the search button
 search.on("click", function (event) {
@@ -34,6 +34,59 @@ clear.on("click", function () {
   $("#history").empty();
 });
 
+$("#history").on("click", ".btn-secondary", function() {
+  console.log("Button Clicked");
+  var thisCity = $(this).attr("name");
+  var geoQueryUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${thisCity}&limit=1&appid=${APIKey}`;
+  
+    // Make the fetch request for geo data
+    fetch(geoQueryUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        // Log or use the data as needed
+        console.log(data);
+  
+        // Assuming data[0] contains latitude and longitude information
+        var lat = data[0].lat;
+        var lon = data[0].lon;
+  
+        // Construct the weather query URL using lat and lon
+        var weatherQueryURL = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=${APIKey}`;
+  
+        // Make the fetch request for weather data
+        fetch(weatherQueryURL)
+          .then(function (response) {
+            return response.json();
+          })
+          .then(function (data) {
+            var today = $("#today").addClass("border border-dark");
+            today.empty();
+            var date = dayjs().format("DD/MM/YYYY");
+            var h2 = $("<h2>");
+            h2.attr("id", "todayHead").addClass("h3").text(
+              thisCity + " (" + date + ") "
+            );
+            today.append(h2);
+             // Apply the weather data to get icon temp, wind and humidity;
+            var iconURL = `https://openweathermap.org/img/wn/${data.list[0].weather[0].icon}.png`;
+            var icon = $("<img alt='weather icon'>");
+            icon.attr('src', iconURL);
+            $("#todayHead").append(icon);
+            var temp = $("<p>");
+            temp.text(`Temp: ${data.list[0].main.temp}°C`)
+            $("#today").append(temp);
+            var wind = $("<p>");
+            wind.text(`Wind: ${data.list[0].wind.speed} KPH`)
+            $("#today").append(wind);
+            var humidity = $("<p>");
+            humidity.text(`Humidity: ${data.list[0].main.humidity}%`)
+            $("#today").append(humidity);
+          });
+      });
+  
+})
 
 function fetchFetch(geoQueryUrl) {
   // Make the fetch request for geo data
@@ -86,7 +139,7 @@ function renderCities() {
   // Loop through the array and display each city
   for (var i = 0; i < storedCities.length; i++) {
     searchedCities.append(
-      `<button type='button' class='btn btn-secondary m-2'>${storedCities[i]}</button>`
+      `<button type='button' name='${storedCities[i]}' class='btn btn-secondary m-2'>${storedCities[i]}</button>`
     );
   }
 }
